f31c95a4774c1f77a82a92fb392f20d0ffecfa1f
Amir Hosein Baqinejad <ambaqinejad@gmail.com>
2021-04-19 22:32:44 +0430

    LSP

--- README.md
+++ README.md
@@ -3,4 +3,5 @@
 ### SOLID Principles
 
 1. Single - Responsibility Principle
-2.  Open - Closed Principle
+2. Open - Closed Principle
+3. Liskov - Substitution Principle
---
+++ Solid/liskovSubstitution.mjs
@@ -0,0 +1,70 @@
+class Rectangle {
+    constructor(height, width) {
+        this._height = height;
+        this._width = width;
+    }
+
+    get area() {
+        return this._height * this._width
+    }
+
+    get height() {
+        return this._height;
+    }
+
+    get width() {
+        return this._width;
+    }
+
+    set height(value) {
+        this._height = value;
+    }
+
+    set width(value) {
+        this._width = value;
+    }
+
+    toString() {
+        return `Height x Width: ${this._height} x ${this._width}`
+    }
+}
+
+class Square extends Rectangle {
+    constructor(size = 5) {
+        super(size, size);
+    }
+
+    get height() {
+        return this._height;
+    }
+
+    get width() {
+        return this._width;
+    }
+
+    set width(value) {
+        this._height = this._width = value;
+    }
+
+    set height(value) {
+        this._height = this._width = value;
+    }
+}
+
+const useIt = function(rectangle) {
+    const width = rectangle.width;
+    rectangle.height = 3;
+    console.log(`I assume I got are equal to ${width*3}
+        but I got ${rectangle.area}`)
+}
+
+const rectangle = new Rectangle(10, 5);
+const square = new Square();
+useIt(rectangle);
+useIt(square);
+// console.log(rectangle.toString())
+// rectangle.height = 20
+// console.log(rectangle.toString())
+// console.log(square.toString())
+// square.height = 10
+// console.log(square.toString())
